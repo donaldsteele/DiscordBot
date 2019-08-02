e79eedf21e666e5f5dad4c64aef24b7f6d9e628f
Don Steele <coachsteele@nafocyberpatriot.com>
2019-08-02 14:10:56 -0400

    Switched to useing charlottedunois/yasmin since it is more feature rich. Improved project structure

---
+++ src/DiscordBot/Commands.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DiscordBot;
+
+class Commands
+{
+    static function echo(DataPacket $dataPacket)
+    {
+        return 'said: ' . $dataPacket->message;
+    }
+
+    static function eightball(DataPacket $dataPacket)
+    {
+        $choices = [
+            'It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes, definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'Most likely',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            'Don\'t count on it',
+            'My reply is no',
+            'My sources say no',
+            'Outlook not so good',
+            'Very doubtful',
+        ];
+
+        return $choices[array_rand($choices)];
+    }
+
+    static function hasMethod($methodName)
+    {
+
+        return method_exists(__CLASS__, $methodName);
+
+    }
+}
---
+++ src/DiscordBot/DataPacket.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DiscordBot;
+
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\Models\Message;
+
+class DataPacket
+{
+    public $command;
+    public $message;
+    public $guild_id;
+    public $author_id;
+    public $channel_id;
+
+    function __construct(Client $client, Message $message)
+    {
+
+
+        $bits = preg_split('/ /', $message, -1, PREG_SPLIT_NO_EMPTY);
+        //print_r($bits);
+        if (count($bits) < 2) {
+            return;
+        }
+
+
+        $command = array_shift($bits);
+        $this->message = implode(" ", $bits);
+        print_r($this->message);
+
+        if ($command[0] == '!') {
+            $command = substr($command, 1);
+            if (Commands::hasMethod($command)) {
+                $this->command = $command;
+            }
+        }
+
+        $this->guild_id = $message->guild;
+        $this->channel_id = $message->channel;
+        $this->author_id = $message->author;
+    }
+
+
+}
